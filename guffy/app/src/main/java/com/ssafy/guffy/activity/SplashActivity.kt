7a1be5fe7851ed446d6d5dcb3422ed3b7c9c5aaf
JungeunPark <pje8845@naver.com>
2022-11-17 10:30:24 +0900

    initial project

---
+++ guffy/app/src/main/java/com/ssafy/guffy/activity/SplashActivity.kt
@@ -0,0 +1,12 @@
+package com.ssafy.guffy.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.ssafy.guffy.R
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+    }
+}
