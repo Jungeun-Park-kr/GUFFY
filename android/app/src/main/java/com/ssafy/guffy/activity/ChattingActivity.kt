59e12f8043b2813440c3f420f0fdcd3e7f50dcca
jungeun <pje8845@naver.com>
2022-11-22 09:41:21 +0900

    [Add] #47 채팅 불러오기 기능 추가

--- android/app/src/main/java/com/ssafy/guffy/activity/ChattingActivity.kt
+++ android/app/src/main/java/com/ssafy/guffy/activity/ChattingActivity.kt
@@ -2,17 +2,26 @@ package com.ssafy.guffy.activity
 
 import android.annotation.SuppressLint
 import android.os.Build
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.TextUtils
 import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.annotation.RequiresApi
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.google.firebase.database.*
 import com.google.firebase.database.ktx.database
 import com.google.firebase.ktx.Firebase
 import com.ssafy.guffy.Adapter.ChattingAdapter
 import com.ssafy.guffy.databinding.ActivityChattingBinding
+import com.ssafy.guffy.databinding.ItemChatMessageBinding
 import com.ssafy.guffy.dto.ChattingItemDto
+import java.text.SimpleDateFormat
+import java.util.*
+
 
 private lateinit var binding: ActivityChattingBinding
 private const val TAG = "ChattingActivity 구피"
@@ -20,10 +29,12 @@ private const val TAG = "ChattingActivity 구피"
 class ChattingActivity : AppCompatActivity() {
     private var messageList = mutableListOf<ChattingItemDto>()
     private lateinit var chattingAdapter: ChattingAdapter
+    private lateinit var chatRecyclerView: RecyclerView
     private lateinit var thisChattingRoomRef : DatabaseReference
+    private lateinit var childEventListener:ChildEventListener
     //lateinit var firebase: FirebaseDatabase // firebase 데이터베이스 관리 객체참조변수
     lateinit var friendNickname: String
-    var myNickName = "지워니" // 내 닉네임은 저장해두자. String에
+    var myNickName = "" // 내 닉네임은 저장해두자. String에
     lateinit var chattingRoomId: String
 
     @RequiresApi(Build.VERSION_CODES.O)
@@ -34,38 +45,19 @@ class ChattingActivity : AppCompatActivity() {
 
 
         // main acitivity 에서 친구이름 받아오기
+/*        myNickName = "지워니"
         friendNickname = "쩡으니"
+        chattingRoomId = "1"*/
+
+        myNickName = "쩡으니" // 내 닉네임은 저장해두자. String에
+        friendNickname = "지워니"
         chattingRoomId = "1"
-        /* friendNickname = "지워니"
-         chattingRoomId = "1"
-         var myNickName = "쩡으니" // 내 닉네임은 저장해두자. String에
- */
 
-        initView();
-
-
-
-
-//        thisChattingRoomRef.addValueEventListener(object :ValueEventListener{
-//            override fun onDataChange(snapshot: DataSnapshot) {
-//                messageList.clear()
-//                for (chat in snapshot.children){
-//                    messageList.add(chat.getValue(ChattingItemDto::class.java)!!)
-//                }
-//                Log.d(TAG, "onDataChange: ${messageList}")
-//
-//                chattingAdapter.insertList(messageList)
-//                chattingAdapter.notifyItemInserted(messageList.size)
-//                binding.chatRecyclerView.smoothScrollToPosition(messageList.size)
-//            }
-//
-//            override fun onCancelled(error: DatabaseError) {
-//                Log.d(TAG, "onCancelled: Fail to read value")
-//            }
-//        })
+        initView()
+        initFirebase()
     }
 
-    fun initView() {
+    private fun initView() {
         // 액션바 보이게 해주고
         supportActionBar?.show()
         //액션바에 친구 이름 넣어주기
@@ -73,7 +65,9 @@ class ChattingActivity : AppCompatActivity() {
 
         // 어댑터 생성 및 연결
         chattingAdapter = ChattingAdapter(messageList, friendNickname)
-        binding.chatRecyclerView.adapter = chattingAdapter
+        chatRecyclerView = binding.chatRecyclerView
+        chatRecyclerView.layoutManager = LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
+        chatRecyclerView.adapter = chattingAdapter
 
         // 전송 버튼 클릭
         binding.chattingSendBtn.setOnClickListener {
@@ -84,10 +78,11 @@ class ChattingActivity : AppCompatActivity() {
 
                 // Realtime database에 전송
                 Log.d(TAG, "onCreate: db에 새로운 메세지 전송")
-                thisChattingRoomRef.push().setValue(chattingItem)
+                thisChattingRoomRef.push().setValue(chattingItem).addOnSuccessListener {
+                    // 성공시 edit text 지우기
+                    binding.chattingMessageEt.setText("")
+                }
 
-                // edit text 지우기
-                binding.chattingMessageEt.setText("")
             }
 
         }
@@ -96,38 +91,29 @@ class ChattingActivity : AppCompatActivity() {
 
     fun initFirebase() {
         // firebase 데이터베이스 관리 객체 얻어오기
-       /* val firebase = FirebaseDatabase.getInstance()
-        thisChattingRoomRef = firebase.reference.child("chattingRoomId").child(chattingRoomId)
-*/
         thisChattingRoomRef = Firebase.database.getReference("chattingRoomId").child(chattingRoomId)
-        var childEventListener = object : ChildEventListener{
+
+        childEventListener = object : ChildEventListener{
+            @SuppressLint("NotifyDataSetChanged")
             override fun onChildAdded(snapshot: DataSnapshot, previousChildName: String?) {
-                /*Log.d(TAG, "onChildAdded: $snapshot")
-//                val chattingItem = snapshot.getValue(ChattingItemDto::class.java)
-                val chattingItem = snapshot.getValue<ChattingItemDto>()
-                if (chattingItem != null) {
-                    Log.d(TAG, "onChildAdded: ${snapshot}")
-                    val chattingItem = snapshot.getValue<ChattingItem>()!!
-                    chattingItem.firebaseKey = snapshot.key ?: ""
-                    Log.d(TAG, "onChildAdded: ${chattingItem}")
+                Log.d(TAG, "onChildAdded: $snapshot")
+                var chattingItem = snapshot.getValue(ChattingItemDto::class.java)!!
+                chattingItem.uid = snapshot.key ?: ""
+                Log.d(TAG, "id: ${chattingItem.uid}")
 
-
-                    messageList.add(chattingItem)
-                    //notifyDatasetChanged() 사용 지양
-                    //chattingAdapter.notifyItemInserted(messageList.size)
-                    chattingAdapter.notifyDataSetChanged()
-                    binding.chatRecyclerView.smoothScrollToPosition(messageList.size)
-                }*/
-
+                messageList.add(chattingItem)
+                //notifyDatasetChanged() 사용 지양
+                chattingAdapter.notifyItemInserted(messageList.size-1)
+                binding.chatRecyclerView.smoothScrollToPosition(messageList.size-1)
             }
 
+            @RequiresApi(Build.VERSION_CODES.N)
             override fun onChildChanged(snapshot: DataSnapshot, previousChildName: String?) {
             }
 
+            @RequiresApi(Build.VERSION_CODES.N)
             @SuppressLint("NotifyDataSetChanged")
             override fun onChildRemoved(snapshot: DataSnapshot) {
-                chattingAdapter.notifyDataSetChanged()
-                binding.chatRecyclerView.smoothScrollToPosition(messageList.size)
             }
 
             override fun onChildMoved(snapshot: DataSnapshot, previousChildName: String?) {
@@ -139,7 +125,5 @@ class ChattingActivity : AppCompatActivity() {
         }
 
         thisChattingRoomRef.addChildEventListener(childEventListener)
-
-
     }
 }
